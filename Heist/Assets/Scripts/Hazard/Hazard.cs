1835e0281dcd1b2e5702e15ffc36d5ffab0ce975
david <abc>
2019-01-25 12:17:37 -0500

    UI Manager for individual players started (75%)

--- Heist/Assets/Scripts/Hazard/Hazard.cs
+++ Heist/Assets/Scripts/Hazard/Hazard.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Hazard
 {
     [RequireComponent(typeof(Collider))]
-    public class Hazard : MonoBehaviour
+    public class Hazard : Game.Item 
     {
         public virtual void Place(Vector3 position)
         {
--- Heist/Assets/Scripts/UI/UIManager.cs
+++ Heist/Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -9,7 +11,20 @@ namespace UI
 
         [SerializeField] private TextMeshProUGUI _playerHint;
 
-        public void ShowPickup(Pickup.Pickup.PickupType pickupType, bool overPickup)
+        [SerializeField] List<GameObject> PlayerUI;
+        [SerializeField] List<GameObject> HealthUI;
+        [SerializeField] Text GoldUI;
+        [SerializeField] List<GameObject> ItemUI;
+        [SerializeField] private GameObject curItemUi;
+        private int curWeap;
+        private Character.Inventory inv;
+
+        public void CreateUI(int i)
+        {
+            PlayerUI[i].SetActive(true);
+        }
+
+        public void ShowPickup(Pickup.PickupType pickupType, bool overPickup)
         {
             if (!overPickup)
             {
@@ -18,16 +33,62 @@ namespace UI
             }
             switch (pickupType)
             {
-                case Pickup.Pickup.PickupType.Weapon:
+                case Pickup.PickupType.Weapon:
                     _playerHint.text = TextHelper.PickupWeapon;
                     break;
-                case Pickup.Pickup.PickupType.Trap:
+                case Pickup.PickupType.Trap:
                     _playerHint.text = TextHelper.PickupTrap;
                     break;
-                case Pickup.Pickup.PickupType.Gold:
+                case Pickup.PickupType.Gold:
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(pickupType), pickupType, null);
+            }
+        }
+        public void UpdateHealth(int hp)
+        {
+            for (int i = 0; i < HealthUI.Count; i++)
+            {
+                HealthUI[i].SetActive(i < hp);
+            }
+        }
+
+        public void UpdateGold(int goldA)
+        {
+            GoldUI.text = goldA.ToString();
+        }
+
+        public void UpdateWeapon(Game.Item item)
+        {
+            if (item is Weapon.Weapon)
+            {
+                if (item is Weapon.StunGun)
+                {
+                    curItemUi.SetActive(false);
+                    curItemUi = ItemUI[0];
+                    curItemUi.SetActive(true);
+                }
+                else if (item is Weapon.Baton)
+                {
+                    curItemUi.SetActive(false);
+                    curItemUi = ItemUI[1];
+                    curItemUi.SetActive(true);
+                }
+            }
+            else if (item is Hazard.Hazard)
+            {
+                if (item is Hazard.ElectricField)
+                {
+                    curItemUi.SetActive(false);
+                    curItemUi = ItemUI[2];
+                    curItemUi.SetActive(true);
+                }
+                else if (item is Hazard.LethalLaser)
+                {
+                    curItemUi.SetActive(false);
+                    curItemUi = ItemUI[3];
+                    curItemUi.SetActive(true);
+                }
             }
         }
     }

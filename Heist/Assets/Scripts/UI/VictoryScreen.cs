6f7875246f2ddf7cda63a336aeaada8af2002ec6
david <kc16rr@brocku.ca>
2019-02-04 15:37:15 -0500

    B A K E D

---
+++ Heist/Assets/Scripts/UI/VictoryScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryScreen : MonoBehaviour
+{
+    [SerializeField] List<Text> playerNames;
+    [SerializeField] List<GameObject> playerImages;
+
+    void Initialize(List<string> PN, List<GameObject> PI)
+    {
+        for (int i = 0; i < PN.Count; i++)
+        {
+            playerNames[i].text = PN[i];
+            playerImages[i] = PI[i];
+        }
+        for (int i = PN.Count; i < 4; i++)
+        {
+            Destroy(playerNames[i]);
+            Destroy(playerImages[i]);
+        }
+    }
+
+
+
+}

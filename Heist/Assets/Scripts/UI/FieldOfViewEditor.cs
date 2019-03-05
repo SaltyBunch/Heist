3906991db6a3dc616b8dc9da5bc87cc2b7b05dca
david <1243>
2019-03-04 19:34:36 -0500

    line of sight done

--- Heist/Assets/Scripts/UI/FieldOfViewEditor.cs
+++ Heist/Assets/Scripts/UI/FieldOfViewEditor.cs
@@ -19,6 +19,11 @@ namespace UI
             Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
             Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
+            Handles.color = Color.red;
+            foreach (Transform visibleTarget in fow.visibleTargets)
+            {
+                Handles.DrawLine(fow.transform.position, visibleTarget.position);
+            }
         }
 
 

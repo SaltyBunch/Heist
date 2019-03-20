0bf1a212d8e00c800dc5dac2db0c2f78706e8d0c
HiRoS-neko <hiros.neko@gmail.com>
2019-03-20 15:39:05 -0400

    Add Pickup Spawner

---
+++ Heist/Assets/Scripts/Pickup/PickupSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pickup
+{
+    public class PickupSpawner : MonoBehaviour
+    {
+        [SerializeField] private List<Pickup> _pickups;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            var i = Random.Range(0, _pickups.Count);
+
+            Instantiate(_pickups[i], transform.position, transform.rotation, null);
+
+            Destroy(this.gameObject);
+        }
+    }
+}

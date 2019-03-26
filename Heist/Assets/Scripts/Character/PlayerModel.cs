a0a2ec550ff5e93a22e94fe291c35ffff40c1ab5
HiRoS-neko <hiros.neko@gmail.com>
2019-03-26 17:28:15 -0400

    Fix jb dash, adjusted player camera

--- Heist/Assets/Scripts/Character/PlayerModel.cs
+++ Heist/Assets/Scripts/Character/PlayerModel.cs
@@ -6,11 +6,22 @@ namespace Character
 {
     public class PlayerModel : MonoBehaviour
     {
+        public enum FacesState
+        {
+            Blink,
+            Defeat,
+            Idle,
+            Smile,
+            Speak,
+            Stun
+        }
+
         [SerializeField] private List<SkinnedMeshRenderer> _characterSkinnedMeshRenderers;
 
         [SerializeField] private MeshRenderer _face;
 
         [SerializeField] private List<Texture2D> _faces;
+        [SerializeField] private FacesState _facesState = FacesState.Idle;
 
         [SerializeField] private int _playerNumber = -1;
 
@@ -48,21 +59,31 @@ namespace Character
                 var v = _colors[_playerNumber];
                 v.a = Mathf.Clamp(_modelAlpha, 0, 0.6f);
                 UpdateColor(v);
+                UpdateFace(v);
             }
             else
             {
                 UpdateColor(Color.clear);
+                UpdateFace(Color.clear);
             }
+        }
+
+        private void UpdateFace(Color color)
+        {
+            _face.GetPropertyBlock(_prop);
+            _prop.SetColor("_OccludedColor", color);
+            _prop.SetTexture("_MainTex", _faces[(int) _facesState]);
+            _face.SetPropertyBlock(_prop);
         }
 
         private void UpdateColor(Color color)
         {
-                foreach (var character in _characterSkinnedMeshRenderers)
-                {
-                    character.GetPropertyBlock(_prop);
-                    _prop.SetColor("_OccludedColor", color);
-                    character.SetPropertyBlock(_prop);
-                }
+            foreach (var character in _characterSkinnedMeshRenderers)
+            {
+                character.GetPropertyBlock(_prop);
+                _prop.SetColor("_OccludedColor", color);
+                character.SetPropertyBlock(_prop);
+            }
         }
 
         public void SetPlayer(int playerNumber)

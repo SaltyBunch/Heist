2b2396ab867789b59326f7dce744075c856968db
davdi <134>
2019-04-02 11:50:20 -0400

    update text helper

--- Heist/Assets/Scripts/UI/TextHelper.cs
+++ Heist/Assets/Scripts/UI/TextHelper.cs
@@ -13,5 +13,14 @@ namespace UI
         public static string OpenMiniVault => "Press <#0000FF>x <#FFFFFF>to open safe"; //todo placeholder for now
         public static string TakeGold => "Press <#0000FF>x <#FFFFFF>to take gold";
         public static string DisableHazard => "Press <#0000FF>x <#FFFFFF>to disable hazard";
+        public static string BlueKeyPickUp => "<#395BFF>You Picked up Blue key";
+        public static string RedKeyPickUp => "<#FF1100>You Picked up Red key";
+        public static string YellowKeyPickUp => "<#FFCC21>You Picked up Yellow key";
+        public static string StairUnlocked => "Stairs have been unlocked"; //shown for everyone pls
+        public static string RequireRedKey => "You require the <#FF1100>Red <#FFFFFF>Key";
+        public static string RequireYellowKey => "You require the <#FFCC21>Yellow <#FFFFFF>Key";
+        public static string RequireBlueKey => "You require the <#395BFF>Blue <#FFFFFF>Key";
+        public static string RequireBothKeys => "You require the <#FF1100>Red <#FFFFFF>and <#FFCC21>Yellow <#FFFFFF>Key";
+        public static string LockDownNotif => "<#FF1100>Lock Down Will Initiate in 2 Minutes"; //Shown for everyone pls
     }
 }

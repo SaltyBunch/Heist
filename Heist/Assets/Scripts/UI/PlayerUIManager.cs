398c0f2504ab1aab3ccf36b44b2c013bbfee185c
HiRoS-neko <hiros.neko@gmail.com>
2019-04-06 19:46:56 -0400

    add a removal of controlhint

--- Heist/Assets/Scripts/UI/PlayerUIManager.cs
+++ Heist/Assets/Scripts/UI/PlayerUIManager.cs
@@ -239,6 +239,8 @@ namespace UI
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
+
+            StartCoroutine(ClearHintIn(2, _controlHint));
         }
 
         public void NeedsBothKeys()
